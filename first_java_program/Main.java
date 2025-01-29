9715e0cadb86c02b7b4fbe73c4b4538a0048b0d0
Amalaasish <139550073+AMALVAASISH@users.noreply.github.com>
2025-01-29 17:42:23 +0530

    did first video till intellij ide setup

---
+++ first_java_program/Main.java
@@ -0,0 +1,27 @@
+// package dsa_java.first_java_program;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+// ==================================
+
+
+//  package dsa_java.first_java_program;
+
+//  public class Main{
+// 	public static void main(String[] args){
+// 		System.out.println(args[1]);
+// 	}	
+// }
+
+// commands that are run
+
+// javac dsa_java/first_java_program/Main.java
+// java dsa_java.first_java_program.Main 30 amal
+
+
+// Java -d . Main.java
+// (the -d is used for setting a folder to create the .class file, . creates it in the current directory)
