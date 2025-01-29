ab970a2a0da4d4ceff4bee19ec26fdf35a8a5336
AMALVAASISH <amalvaasish2000@gmail.com>
2025-01-29 22:35:37 +0530

    till primitive datatypes in java

--- first_java_program/Main.java
+++ first_java_program/Main.java
@@ -1,5 +1,5 @@
 // package dsa_java.first_java_program;
-
+//
 // public class Main {
 //     public static void main(String[] args) {
 //         System.out.println("Hello, World!");
@@ -10,11 +10,11 @@
 
 
 //  package dsa_java.first_java_program;
-
+//
 //  public class Main{
 // 	public static void main(String[] args){
 // 		System.out.println(args[1]);
-// 	}	
+// 	}
 // }
 
 // commands that are run
@@ -25,3 +25,39 @@
 
 // Java -d . Main.java
 // (the -d is used for setting a folder to create the .class file, . creates it in the current directory)
+
+//=============================================================
+//
+//  package dsa_java.first_java_program;
+//  import java.util.Scanner;
+//
+//  public class Main{
+// 	public static void main(String[] args){
+// 		Scanner input = new Scanner(System.in);
+//        System.out.println(input.next()); // next is used for string and nextint is used for integer values
+// 	}
+// }
+
+
+//  Here scanner is a class that allows us to take inputs.
+//
+//  System.in is used to insert values from the command line
+//
+//  nextInt takes in only integer values
+//  Use next() to take in string values and then print them
+//  nextLine is used for inputting an entire line
+
+
+// primitive datatypes in java
+
+package dsa_java.first_java_program;
+
+public class Main{
+    public static void main(String[] args) {
+        int rollno = 3;
+
+    }
+}
+
+// time - 48.17
+
